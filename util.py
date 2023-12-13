1eb3a4e4c519ddcb31d2690235e5619b6bde5222
Alsyahrir <153171771+Alsyahrir@users.noreply.github.com>
2023-12-13 18:46:59 +0700

    Update util.py

--- util.py
+++ util.py
@@ -1,47 +1,25 @@
 import base64
-
 import streamlit as st
 from PIL import ImageOps, Image
 import numpy as np
+import io
 
-
-def set_background(image_file):
+def classify(image_data, model, class_names):
     """
-    This function sets the background of a Streamlit app to an image specified by the given image file.
-
-    Parameters:
-        image_file (str): The path to the image file to be used as the background.
-
-    Returns:
-        None
-    """
-    with open(image_file, "rb") as f:
-        img_data = f.read()
-    b64_encoded = base64.b64encode(img_data).decode()
-    style = f"""
-        <style>
-        .stApp {{
-            background-image: url(data:image/png;base64,{b64_encoded});
-            background-size: cover;
-        }}
-        </style>
-    """
-    st.markdown(style, unsafe_allow_html=True)
-
-
-def classify(image, model, class_names):
-    """
-    This function takes an image, a model, and a list of class names and returns the predicted class and confidence
+    This function takes raw image data, a model, and a list of class names and returns the predicted class and confidence
     score of the image.
 
     Parameters:
-        image (PIL.Image.Image): An image to be classified.
+        image_data (bytes): Raw image data from Streamlit file uploader.
         model (tensorflow.keras.Model): A trained machine learning model for image classification.
         class_names (list): A list of class names corresponding to the classes that the model can predict.
 
     Returns:
         A tuple of the predicted class name and the confidence score for that prediction.
     """
+    # Convert raw image data to PIL Image
+    image = Image.open(io.BytesIO(image_data)).convert('RGB')
+    
     # convert image to (224, 224)
     image = ImageOps.fit(image, (224, 224), Image.Resampling.LANCZOS)
 
@@ -57,7 +35,6 @@ def classify(image, model, class_names):
 
     # make prediction
     prediction = model.predict(data)
-    # index = np.argmax(prediction)
     index = 0 if prediction[0][0] > 0.95 else 1
     class_name = class_names[index]
     confidence_score = prediction[0][index]
