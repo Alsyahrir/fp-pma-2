4277f8265fb186e7cd577368c015069174615ff3
Alsyahrir <153171771+Alsyahrir@users.noreply.github.com>
2023-12-13 14:46:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import streamlit as st
 from keras.models import load_model
 from keras.preprocessing import image as keras_image
 from PIL import Image
-import numpy as np
 
 from util import classify, set_background
 
@@ -23,20 +22,14 @@ model = load_model('./model/harist.h5')
 # load class names
 with open('./model/labels.txt', 'r') as f:
     class_names = [a[:-1].split(' ')[1] for a in f.readlines()]
-    f.close()
 
 # display image
 if file is not None:
     image = Image.open(file).convert('RGB')
     st.image(image, use_column_width=True)
 
-    # Preprocess the image for the model
-    image = image.resize((224, 224))  # Resize the image to match the model's expected sizing
-    image_array = keras_image.img_to_array(image)
-    image_array = np.expand_dims(image_array, axis=0)
-    image_array /= 255.0  # Normalize the image
-
     # classify image
+    image_array = keras_image.img_to_array(keras_image.load_img(file, target_size=(224, 224)))
     class_name, conf_score = classify(image_array, model, class_names)
 
     # write classification
