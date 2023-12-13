cad570fe36410686299cdc7172244f56ce1e47a1
Alsyahrir <153171771+Alsyahrir@users.noreply.github.com>
2023-12-13 14:50:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,40 @@
 import streamlit as st
 from keras.models import load_model
-from keras.preprocessing.image import img_to_array, load_img
+from keras.preprocessing import image as keras_image
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from PIL import Image
 
-from util import classify, set_background
-
-set_background('./bgs/bg5.png')
-
-# set title
-st.title('Pneumonia classification')
-
-# set header
-st.header('Please upload a chest X-ray image')
-
-# upload file
-file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
-
-# load classifier
+# Load your model
 model = load_model('./model/harist.h5')
 
-# load class names
+# Load class names
 with open('./model/labels.txt', 'r') as f:
     class_names = [a[:-1].split(' ')[1] for a in f.readlines()]
 
-# display image
+# Set title and header
+st.title('Pneumonia Classification')
+st.header('Upload a chest X-ray image')
+
+# Upload file through Streamlit
+file = st.file_uploader('Choose a file', type=['jpeg', 'jpg', 'png'])
+
+# Display image and perform classification
 if file is not None:
-    try:
-        image = Image.open(file).convert('RGB')
-        st.image(image, use_column_width=True)
+    image = Image.open(file).convert('RGB')
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
 
-        # classify image
-        image_array = img_to_array(image)
-        image_array = image_array.reshape((1,) + image_array.shape)
-        class_name, conf_score = classify(image_array, model, class_names)
+    # Preprocess the image for your model
+    img_array = keras_image.img_to_array(image)
+    img_array = img_array / 255.0  # Normalize the image
+    img_array = keras_image.smart_resize(img_array, (224, 224))
+    img_array = preprocess_input(img_array)
+    img_array = np.expand_dims(img_array, axis=0)
 
-        # write classification
-        st.write("## {}".format(class_name))
-        st.write("### score: {}%".format(int(conf_score * 1000) / 10))
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
+    # Make predictions
+    predictions = model.predict(img_array)
+    decoded_predictions = decode_predictions(predictions, top=1)[0]
+    class_name, conf_score = decoded_predictions[0][1], decoded_predictions[0][2]
+
+    # Write classification results
+    st.write("## Prediction: {}".format(class_name))
+    st.write("### Confidence Score: {}%".format(round(conf_score * 100, 2)))
