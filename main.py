c251262f61d9fb15821d02855bd5e728c646556a
Alsyahrir <153171771+Alsyahrir@users.noreply.github.com>
2023-12-13 18:32:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
-from tensorflow.keras.models import load_models
+from tensorflow.keras.models import load_model  # Fix the typo here
 from PIL import Image
 import numpy as np
 from util import classify, preprocess_image, set_background
