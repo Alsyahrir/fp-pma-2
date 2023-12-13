bd22ce387ea16d25caa50f881450bb67d9bda53b
Alsyahrir <153171771+Alsyahrir@users.noreply.github.com>
2023-12-13 14:45:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from keras.models import load_model
+from keras.preprocessing import image as keras_image
 from PIL import Image
 import numpy as np
 
 from util import classify, set_background
-
 
 set_background('./bgs/bg5.png')
 
@@ -30,8 +30,14 @@ if file is not None:
     image = Image.open(file).convert('RGB')
     st.image(image, use_column_width=True)
 
+    # Preprocess the image for the model
+    image = image.resize((224, 224))  # Resize the image to match the model's expected sizing
+    image_array = keras_image.img_to_array(image)
+    image_array = np.expand_dims(image_array, axis=0)
+    image_array /= 255.0  # Normalize the image
+
     # classify image
-    class_name, conf_score = classify(image, model, class_names)
+    class_name, conf_score = classify(image_array, model, class_names)
 
     # write classification
     st.write("## {}".format(class_name))
